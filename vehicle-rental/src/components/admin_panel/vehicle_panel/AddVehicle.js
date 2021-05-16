1b5c82d4467ad08d6b1f1fe1aafd31d35a78a8c3
Anna Foltyniewicz <foltyniewicz.anna@gmail.com>
2021-05-16 11:52:57 +0200

    Dodanie zarządzania pojazdami - panel administratora

---
+++ vehicle-rental/src/components/admin_panel/vehicle_panel/AddVehicle.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react'
+import Axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../style.css'
+
+const AddVehicle = (props) => {
+
+    const [name, setName] = useState('')
+    const [model, setModel] = useState('')
+    const [type, setType] = useState('')
+    const [engine, setEngine] = useState('')
+    const state = "dostępny"
+
+    const [vehicleData, setVehicleData] = useState([])
+
+    const submitAddVehicle = () => { // to instert data
+
+        Axios.post('http://localhost:3001/insertVehicle', {
+            name: name,
+            model: model,
+            type: type,
+            engine: engine,
+            state: state
+        });
+    
+        if(name !== "" && model !== "" && type !== "" && engine !== "" && state !== ""){
+            setVehicleData([
+                ...vehicleData, 
+                {   
+                    Name: name,
+                    Model: model,
+                    Type: type,
+                    Engine_capacity: engine,
+                    State: state
+                }
+            ]);
+            alert("Poprawnie dodano użytkownika :)")
+            props.refresh()
+        } else {
+            alert("Niektóre dane są puste :/")
+            console.log("undefined data of repair :(")
+        }
+    }
+
+    return (
+        <div className="container option_panel_box d-flex justify-content-center">
+            <table className="col-10 center-block text-center">
+                <tr>
+                    <th colspan="2"><h5>Dodaj użytkownika</h5></th>
+                </tr>
+                <tr>
+                    <td>Nazwa:</td>
+                    <td>
+                        <input type="text" onChange={(e) => {
+                            setName(e.target.value)
+                        }}></input>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Model:</td>
+                    <td>
+                        <input type="text" onChange={(e) => {
+                            setModel(e.target.value)
+                        }}></input>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Typ:</td>
+                    <td>
+                        <select class="form-select col-12" onChange={(e) => {
+                            setType(e.target.value)
+                        }}>
+                            <option selected>...Wybierz typ pojazdu</option>
+                            <option value="Sedan">Sedan</option>
+                            <option value="SUV">SUV</option>
+                            <option value="Hatchback">Hatchback</option>
+                            <option value="VAN">VAN</option>
+                            <option value="Coupe">Coupe</option>
+                            <option value="Camper">Camper</option>
+                            <option value="Cabriolet">Cabriolet</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Pojemność silnika:</td>
+                    <td>
+                        <input type="number" onChange={(e) => {
+                            setEngine(e.target.value)
+                        }}></input>    
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="button" className="btn btn-success" value="dodaj użytkownika" onClick={submitAddVehicle}></input></td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+export default AddVehicle;
+
+
